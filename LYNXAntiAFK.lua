c674220f6e981883ec54f70244c621f39b195ae6
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-06 11:41:21 +0800

    Update LYNXAntiAFK.lua

--- LYNXAntiAFK.lua
+++ LYNXAntiAFK.lua
@@ -1,1 +1,18 @@
-
+local GC = getconnections or get_signal_cons
+local Players = game:GetService("Players")
+if GC then
+    for i, v in pairs(GC(Players.LocalPlayer.Idled)) do
+        if v["Disable"] then
+            v["Disable"](v)
+        elseif v["Disconnect"] then
+            v["Disconnect"](v)
+        end
+    end
+else
+    Players.LocalPlayer.Idled:Connect(
+        function()
+            VirtualUser:CaptureController()
+            VirtualUser:ClickButton2(Vector2.new())
+        end
+    )
+end
