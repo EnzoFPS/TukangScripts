be3ebea34a54c3f829ce6da2195a540dddf5159b
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-07 14:02:17 +0800

    Create LYNXRejoinServer.lua

---
+++ LYNXRejoinServer.lua
@@ -0,0 +1,2 @@
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, lp)
+        notify("Teleporting...", "Teleport in progress, please wait...", "rbxassetid://6962520787", 5)
