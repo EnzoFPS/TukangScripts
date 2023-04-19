595a47cf30de3eeba3d1d602a6fd371174415280
EnzoBRZ <79358946+TukangTechnology@users.noreply.github.com>
2023-04-19 17:15:22 +0800

    Update LYNXPingSpoof.lua

--- LYNXPingSpoof.lua
+++ LYNXPingSpoof.lua
@@ -1,13 +1,14 @@
 local PerformanceStats = game:GetService("CoreGui"):WaitForChild("RobloxGui"):WaitForChild("PerformanceStats");
 local PingLabel;
 for I, Child in next, PerformanceStats:GetChildren() do
-    if Child.StatsMiniTextPanelClass.TitleLabel.Text == "Mem" then
+    if Child.StatsMiniTextPanelClass.TitleLabel.Text == "Ping" then
         PingLabel = Child.StatsMiniTextPanelClass.ValueLabel;
         break;
     end;
 end;
 
-local text = "600 MB";
+local text = "1 ms";
 PingLabel:GetPropertyChangedSignal("Text"):Connect(function()
-    PingLabel.Text = "600 MB";
+    PingLabel.Text = "1 ms";
 end);
+PingLabel.Text = text;
